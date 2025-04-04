27239e70997f9f7c40770087dc1c5c82cbce0748
sleepy-jelly <kernel2015@naver.com>
2025-04-04 16:19:35 +0900

    Inital commit

---
+++ VuePOC/src/main/java/com/jelly/pb/vuepoc/VuepocApplication.java
@@ -0,0 +1,13 @@
+package com.jelly.pb.vuepoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuepocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuepocApplication.class, args);
+	}
+
+}
---
+++ VuePOC/src/test/java/com/jelly/pb/vuepoc/VuepocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jelly.pb.vuepoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuepocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
