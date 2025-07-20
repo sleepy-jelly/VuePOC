73f6f19d6b4abdf67cfca32c98c99460aa31564c
sleepy-jelly <kernel2015@naver.com>
2025-07-21 08:43:48 +0900

    add contact.vue

---
+++ VuePOC/src/main/VuePOC-Vue/src/views/ContactView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import DefaultLayout from '@/components/layouts/DefaultLayout.vue'
+import Contact from '@/components/Contact.vue'
+</script>
+
+<template>
+    <DefaultLayout>
+        <Contact />
+    </DefaultLayout>
+</template> 
